6c25da5fc72cb2a51fefb49b278ba0425f3efc6b
bizmedia1 <victorisrael539@gmail.com>
2026-05-19 12:55:45 +0100

    Create check-payment.js

---
+++ api/check-payment.js
@@ -0,0 +1,17 @@
+import { payments } from './store';
+
+export default async function handler(req, res) {
+
+const { reference } = req.query;
+
+if (!payments[reference]) {
+return res.status(404).json({
+paid: false
+});
+}
+
+res.status(200).json({
+paid: payments[reference].status === "paid"
+});
+
+}
